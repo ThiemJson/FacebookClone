eb53894f366991bb14e58c44196cbfbbac3847d8
Nguyen Cao Thiem <nguyen_thiem@teneocto.io>
2020-11-27 10:59:09 +0700

    Implement ListView

---
+++ LoginFacebook/ListView.swift
@@ -0,0 +1,21 @@
+//
+//  ListView.swift
+//  LoginFacebook
+//
+//  Created by Teneocto on 11/27/20.
+//  Copyright © 2020 NguyenCaoThiem. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
